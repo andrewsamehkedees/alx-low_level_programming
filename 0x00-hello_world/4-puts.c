d064da21dffadc37bd7f5c1f52891a732f8a6828
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-13 08:10:39 -0700

    test4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/**
- * main gonna print using put
- *
- * return always zero*/
+/* initiate the main method */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+/* returning zero*/
 return (0);
 }

87100d03b8e0ab1c8f140599b0828740936ebb56
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-13 08:51:15 -0700

    all

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main method with print 
- *
- * Return: always return 0
- */
+* main - print using printf
+*
+* Return: Always return 0
+*/
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

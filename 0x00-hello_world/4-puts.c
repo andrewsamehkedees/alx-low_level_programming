ceaac40b2bedf4753bfcbcc528e0d6653b168271
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-13 07:52:41 -0700

    puts5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /*the main functions with int as return type and prints using puts function*/
-int main(void)
-{
+int main(void){
 /*printing using puts*/
 puts("\"Programming is like building a multilingual puzzle");
 /*returning zero after finishing*/
-return (0);
-}
+return (0);}

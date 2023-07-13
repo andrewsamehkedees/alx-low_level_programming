40cd8378cbc85b70a6e31d8eff1319d76c2ac21d
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-13 08:44:50 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main method with print 
+ *
+ * return 0 after finish
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}

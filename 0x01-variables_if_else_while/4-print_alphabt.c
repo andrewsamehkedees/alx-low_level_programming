5f811af5ac50969ba626854916a71c8437fd18cb
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-14 13:44:50 -0700

    all

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - print the alphabets.
+ * main - print the alphabets withou q and e.
  *
  * Return: Always return 0
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alp[26] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 0; i < 52; i++)
+	for (i = 0; i < 24; i++)
 	{
 		putchar(alp[i]);
 	}

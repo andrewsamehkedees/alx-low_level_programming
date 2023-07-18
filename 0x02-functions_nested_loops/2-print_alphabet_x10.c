667d183d06dbc771441f06aaf819172b80d5b7ae
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-18 05:02:57 -0700

    alpha10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in order 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int l;
+
+	for(l = 0; l < 10 ;l++)
+	{
+		char i;
+
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}

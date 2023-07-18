1525ecb498989e4de52e6110478c20128e4984c4
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-18 14:59:59 -0700

    test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,7 @@ void times_table(void)
                 _putchar('0' + product);
             }
             _putchar(',');
-            _putchar('  ');
+            _putchar(' ');
         }
         _putchar('\n');
     }

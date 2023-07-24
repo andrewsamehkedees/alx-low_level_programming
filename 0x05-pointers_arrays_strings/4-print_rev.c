3d5979c0c1ad870f9a4936f8afab4212f4bfe16e
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-24 12:39:43 -0700

    test2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,18 @@
 void print_rev(char *s)
 {
 	int len = 0;
+	int i;
 
 	while (*s != '\0')
 	{
-		len++;
+		longi++;
 		s++;
 	}
-
-	for (int i = len - 1; i >= 0; i--)
+	s--;
+	for (i = len; i > 0; i--)
 	{
-		_putchar(s[i]);
+		_putchar(*s);
+		s--;
 	}
 
 	_putchar('\n');

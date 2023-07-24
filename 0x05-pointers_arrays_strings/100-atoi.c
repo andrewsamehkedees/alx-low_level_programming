f011e9e2bd5e376a30f13b3aadf838fd6aed4f52
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-24 13:11:39 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,19 +7,42 @@
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
+	int i = 0;
+	int sign_count = 0;
+	int result = 0;
+	int length = 0;
+	int first_digit_found = 0;
+	int digit;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-            result = result * 10 + (s[i] - '0');
-        else if (result > 0)
-            break;
-        i++;
-    }
-    return (result * sign);
+	while (s[length] != '\0')
+		length++;
+
+	while (i < length && first_digit_found == 0)
+	{
+		if (s[i] == '-')
+			++sign_count;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+
+			if (sign_count % 2)
+				digit = -digit;
+
+			result = result * 10 + digit;
+
+			first_digit_found = 1;
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+
+			first_digit_found = 0;
+		}
+		i++;
+	}
+
+	if (first_digit_found == 0)
+		return (0);
+
+	return (result);
 }

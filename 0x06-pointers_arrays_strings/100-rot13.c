be05e6b927b1fdaf451f18b765cdef672857f8c3
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-26 15:26:20 -0700

    test

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+/**
+ * rot13 - encodes the string
+ * @s: the string
+ *
+ * Return: string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}

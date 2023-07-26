a154ed2f6c32982f9c9cad77cbb176be34095863
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-26 13:24:45 -0700

    tesrt

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: the destination
+ * @src: the source
+ * @n: maximum numbers
+ *
+ * Return: pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i]
+	dest[i] = '\0';
+
+	return (dest);
+}

871cc44d276a7fe0caf397c38c0f33034fd96eb6
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-07-26 15:28:28 -0700

    test

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+/**
+ * print_number - prints number
+ * @n: the number
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if (num / 10)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}

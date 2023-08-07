7d0c8c4ba7f11de42bf648f662bdf543e6571fc0
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-08-07 02:07:54 -0700

    first

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,13 +13,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 
 	array = mallock(sizeof(char) * size);
-	
+
 	if (array == NULL)
-		return NULL;
+		return (NULL);
 	for (i = 0; i < size; i++)
 		array[i] = c;
-	return array;
+	return (array);
 }

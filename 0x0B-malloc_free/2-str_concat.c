989a742c1ae56e43346216cbb4b6e745437488af
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-08-07 05:27:22 -0700

    third

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *newStr;
-	unsigned int i, j, len1, len2;
+	unsigned int i, j, len1 = 0, len2 = 0;
 
 	if (s1 != NULL)
 		for (len1 = 0; s1[len1]; len1++)

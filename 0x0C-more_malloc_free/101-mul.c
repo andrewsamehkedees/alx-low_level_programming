341a15a5b3a610a262c2504951b04acb36115b64
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-08-10 12:01:59 -0700

    test

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,44 +1,112 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/**
+ * is_digit - checks if a character is a digit
+ * @c: character
+ * Return: 1 or 0
+ */
+int is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
+
+/**
+ * _print - prints a string to stdout
+ * @str: string to print
+ */
+void _print(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+}
+
+/**
+ * mul - multiplies two positive numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: result of multiplication
+ */
+char *mul(char *num1, char *num2)
+{
+	int len1, len2, len, i, j, carry;
+	char *result;
+
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	len = len1 + len2;
+	result = calloc(len + 1, sizeof(char));
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			carry += (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1];
+			result[i + j + 1] = carry % 10;
+			carry /= 10;
+		}
+		result[i] += carry;
+	}
+	for (i = 0; result[i] == 0 && i < len; i++)
+		;
+	for (j = i; j < len; j++)
+		result[j - i] = result[j] + '0';
+	result[len - i] = '\0';
+	return (result);
+}
 
 /**
  * main - multiplies two positive numbers and prints the result
- * @argc: number of arguments
+ * @argc: number of array elements
  * @argv: array
  * Return: 0 or 98
  */
 int main(int argc, char **argv)
 {
-	unsigned long num1, num2;
-	char *result;
+	char *num1, *num2, *result;
 	int i;
 
 	if (argc != 3)
 	{
-		printf("Error\n");
+		_print("Error\n");
 		exit(98);
 	}
-
-	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] < '0' || argv[1][i] > '9')
+	num1 = argv[1];
+	num2 = argv[2];
+	for (i = 0; num1[i]; i++)
+		if (!is_digit(num1[i]))
 		{
-			printf("Error\n");
+			_print("Error\n");
 			exit(98);
 		}
-
-	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] < '0' || argv[2][i] > '9')
+	for (i = 0; num2[i]; i++)
+		if (!is_digit(num2[i]))
 		{
-			printf("Error\n");
+			_print("Error\n");
 			exit(98);
 		}
-
-	num1 = atol(argv[1]);
-
-	num2 = atol(argv[2]);
-
-	result = num1 * num2
-	while (*result)
-		putchar(*result++);
+	result = mul(num1, num2);
+	if (*result == '\0')
+		_putchar('0');
+	else
+		_print(result);
+	_putchar('\n');
+	free(result);
 	return (0);
 }

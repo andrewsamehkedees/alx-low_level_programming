a4cb09654fbbbca5c85e842b5a18d01a550d9d05
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-08-14 11:42:01 -0700

    forth

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,10 +2,10 @@
 #define FUNCTION_LIKE_MACRO_H
 
 #define ABS(x) ({ \
-    int _x = (x); \
-    if (_x < 0) \
-        _x = -_x; \
-    _x; \
+	int _x = (x); \
+	if (_x < 0) \
+		_x = -_x; \
+	_x; \
 })
 
 #endif /* FUNCTION_LIKE_MACRO_H */

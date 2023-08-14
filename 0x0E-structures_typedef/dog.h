4ae1e516c5fd72e7e79fe32ad9f81f81077b143f
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-08-14 10:58:29 -0700

    test

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,14 +6,13 @@
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
- * Description: a new type that defines a dog with its name, age, and owner
+ * Description: a new type names dog with its name, age, and owner
  */
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog;
 
 #endif /* DOG_H */

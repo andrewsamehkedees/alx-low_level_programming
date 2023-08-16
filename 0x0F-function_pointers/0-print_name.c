f7ce5c1ee3afccf5a8fd585573081731943d3852
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-08-16 15:25:51 -0700

    test

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_name - prints a name using a function
+* @name: name
+* @f: function
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

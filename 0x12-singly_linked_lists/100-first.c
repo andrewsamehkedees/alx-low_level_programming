184c10448b8e1c345c798247565f33241ba7d021
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-08-24 11:41:53 -0700

    test

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/**
+ * premain - prints a message before main is executed
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

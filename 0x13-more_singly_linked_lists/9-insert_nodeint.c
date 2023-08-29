84eba9521ed76b6758cfc4e1e87e4ded581322cb
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-08-29 15:47:07 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		while (i < idx - 1 && temp != NULL)
 		{
 			temp = temp->next;
-			if (temp == null)
+			if (temp == NULL)
 				return (NULL);
 		}
 		new->next = temp->next;

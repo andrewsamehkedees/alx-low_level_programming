f7c9c277d21fb1115e2394a99d1d891718e61997
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-09-04 16:45:25 -0700

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,28 +10,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t to_write;
-    mode_t mode;
+	int fd;
+	ssize_t to_write;
+	mode_t mode;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    mode = S_IRUSR | S_IWUSR;
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-    if (fd == -1)
-        return (-1);
+	mode = S_IRUSR | S_IWUSR;
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (to_write = 0; text_content[to_write]; to_write++)
-            ;
-        to_write = write(fd, text_content, to_write);
-        if (to_write == -1)
-            return (-1);
-    }
+	if (text_content != NULL)
+	{
+		for (to_write = 0; text_content[to_write]; to_write++)
+			;
+		to_write = write(fd, text_content, to_write);
+		if (to_write == -1)
+			return (-1);
+	}
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }

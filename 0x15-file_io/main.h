a2b4917cb498b87d20ad1369fff04b53c4d921ce
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-09-04 17:15:11 -0700

    test

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,5 +7,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void write_error_to(int fd_from, int fd_to, char *filename);
+void read_error_to(int fd_from, int fd_to, char *filename);
+void close_error_to(int fd);
 
 #endif /* MAIN_H */

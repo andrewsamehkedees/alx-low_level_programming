385dcd74cd23acb76473d24b3f340f80efb00a98
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-10-26 11:46:54 -0700

    phase 7

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	unsigned int x = 0x76543210;
+	char *c = (char *) &x;
+
+	return (*c == 0x10);
+}

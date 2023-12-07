ef01afc1569447008cab7de436b7534ff3d56445
andrewsamehkedees <andrewsamehkedees@gmail.com>
2023-12-07 15:02:06 -0800

    test

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,15 +8,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t size = 0;
+	const dlistint_t *current = h;
+	size_t size = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-        size++;
-    }
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		size++;
+	}
 
-    return (size);
+	return (size);
 }
